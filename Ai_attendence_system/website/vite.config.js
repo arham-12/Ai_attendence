d5c4ca28bc9648f88ee69d2525ee6ce7cfa9b8be
Taha Asif <tahaasifaqwe@gmail.com>
2025-12-27 18:45:52 +0500

    Updated vite config

--- Ai_attendence_system/website/vite.config.js
+++ Ai_attendence_system/website/vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import mkcert from 'vite-plugin-mkcert';
-import fs from 'fs';
-import path from 'path';
+// import mkcert from 'vite-plugin-mkcert';
+// import fs from 'fs';
+// import path from 'path';
 
 export default defineConfig({
-  plugins: [react(), mkcert()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(process.env.HOME, 'certs/key.pem')),
-      cert: fs.readFileSync(path.resolve(process.env.HOME, 'certs/cert.pem')),
-    },
-    host: true,
-  },
+  plugins: [react()],
+  // server: {
+  //   https: {
+  //     key: fs.readFileSync(path.resolve(process.env.HOME, 'certs/key.pem')),
+  //     cert: fs.readFileSync(path.resolve(process.env.HOME, 'certs/cert.pem')),
+  //   },
+  //   host: true,
+  // },
 });
